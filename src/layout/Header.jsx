00ec5b4eacb4fa616276b1162966507d25bd62a2
Igor Dragun <draggun.ia@gmail.com>
2022-09-14 13:29:25 +0300

    fix links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
   return (
     <nav className="indigo accent-4">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/movies/" className="brand-logo">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://github.com/dragun-igor/movies">Repo</a>
           </li>
         </ul>
       </div>
